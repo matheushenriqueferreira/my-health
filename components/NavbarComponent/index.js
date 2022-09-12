a150dda163b81442cad1d78ee01b8c30aa9faa2a
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-12 19:42:35 -0300

    Criado a estrutura e estilização

--- components/NavbarComponent/index.js
+++ components/NavbarComponent/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Image } from "react-native";
 import { NavbarComponentStyle } from "./styles";
+import hamburgerIcon from '../../assets/image/hamburgerIcon.png';
 
 const NavbarComponent = () => {
   return(
-    <View>
-
+    <View style={NavbarComponentStyle.navbarContainer}>
+      <View style={[NavbarComponentStyle.displayFlex]}>
+        <Text style={NavbarComponentStyle.menuIconContainer}>
+          <Image style={NavbarComponentStyle.hamburgerIcon} source={hamburgerIcon} />
+        </Text>
+        <Text style={NavbarComponentStyle.title}>Minhas vacinas</Text>
+      </View>
     </View>
   );
 }
--- components/NavbarComponent/styles.js
+++ components/NavbarComponent/styles.js
@@ -1,5 +1,36 @@
 import { StyleSheet } from "react-native";
 
 export const NavbarComponentStyle = StyleSheet.create({
+  navbarContainer: {
+    width: '100%',
+    height: 60,
+    backgroundColor: '#C1E7E3',
+    paddingHorizontal: 10,
+    zIndex: 0
+  },
+  
+  displayFlex: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  menuIconContainer: {
+    width: 50,
+    height: '100%'
+  },
+
+  hamburgerIcon: {
+    width: 50,
+    height: 35
+  },
+
+  title: {
+    fontSize: 30,
+    color: '#419ED7',
+    marginLeft: 10
+  }
 
 });
