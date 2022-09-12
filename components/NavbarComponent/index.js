8b2d47d87c10d3ad5051d378529b69536ea4ba2d
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-12 19:10:02 -0300

    Criado arquivos do NavbarComponent

---
+++ components/NavbarComponent/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { NavbarComponentStyle } from "./styles";
+
+const NavbarComponent = () => {
+  return(
+    <View>
+
+    </View>
+  );
+}
+
+export default NavbarComponent;
