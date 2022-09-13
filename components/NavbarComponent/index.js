587c93b77729adc32dd6ff91827a9952bdc5d803
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-12 21:16:21 -0300

    Criado NavbarComponent

--- components/NavbarComponent/index.js
+++ components/NavbarComponent/index.js
@@ -1,16 +1,40 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { NavbarComponentStyle } from "./styles";
 import hamburgerIcon from '../../assets/image/hamburgerIcon.png';
+import line from '../../assets/image/line.png'
+import iconVaccine from '../../assets/image/icon-vaccine.png'
+import iconLogout from '../../assets/image/logout.png'
 
 const NavbarComponent = () => {
+  
+  const handleDrawerNavigator = () => {
+    
+  }
+  
   return(
-    <View style={NavbarComponentStyle.navbarContainer}>
-      <View style={[NavbarComponentStyle.displayFlex]}>
-        <Text style={NavbarComponentStyle.menuIconContainer}>
-          <Image style={NavbarComponentStyle.hamburgerIcon} source={hamburgerIcon} />
-        </Text>
-        <Text style={NavbarComponentStyle.title}>Minhas vacinas</Text>
+    <View style={NavbarComponentStyle.navbar}>
+      <View style={NavbarComponentStyle.navbarContainer}>
+        <View style={[NavbarComponentStyle.displayFlex]}>
+          <TouchableOpacity onPress={() => handleDrawerNavigator()}>
+            <Image style={NavbarComponentStyle.hamburgerIcon} source={hamburgerIcon} />
+          </TouchableOpacity>
+          <Text style={NavbarComponentStyle.title}>Minhas vacinas</Text>
+        </View>
+      </View>
+      <View style={NavbarComponentStyle.drawerNavigator}>
+        <View style={NavbarComponentStyle.drawerNavigatorContainer}>
+          <Text style={[NavbarComponentStyle.drawerText, NavbarComponentStyle.textCenter]}>Olá Matheus</Text>
+          <Image source={line} style={NavbarComponentStyle.lineSize}/>
+          <TouchableOpacity style={NavbarComponentStyle.drawerLinks}>
+            <Image source={iconVaccine} style={NavbarComponentStyle.drawerIcons}/>
+            <Text style={NavbarComponentStyle.drawerText}>Minhas vacinas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={NavbarComponentStyle.drawerLinks}>
+            <Image source={iconLogout} style={NavbarComponentStyle.drawerIcons}/>
+            <Text style={NavbarComponentStyle.drawerText}>Sair</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- components/NavbarComponent/styles.js
+++ components/NavbarComponent/styles.js
@@ -1,12 +1,18 @@
 import { StyleSheet } from "react-native";
 
 export const NavbarComponentStyle = StyleSheet.create({
+  navbar: {
+    width: '100%',
+    height: '100%'
+  },
+  
   navbarContainer: {
+    position: 'absolute',
     width: '100%',
     height: 60,
     backgroundColor: '#C1E7E3',
     paddingHorizontal: 10,
-    zIndex: 0
+    zIndex: 2
   },
   
   displayFlex: {
@@ -15,11 +21,6 @@ export const NavbarComponentStyle = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center'
-  },
-
-  menuIconContainer: {
-    width: 50,
-    height: '100%'
   },
 
   hamburgerIcon: {
@@ -31,6 +32,51 @@ export const NavbarComponentStyle = StyleSheet.create({
     fontSize: 30,
     color: '#419ED7',
     marginLeft: 10
+  },
+
+  drawerNavigator: {
+    position: 'absolute',
+    display: 'none',
+    backgroundColor: '#ADD4D0',
+    width: '50%',
+    height: '100%',
+    zIndex: 1
+  },
+
+  drawerNavigatorContainer: {
+    width: '100%',
+    height: '100%',
+    paddingVertical: 45,
+    paddingHorizontal: 15,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+
+  textCenter: {
+    textAlign: 'center'
+  },
+
+  drawerText: {
+    fontSize: 20,
+    color: '#419ED7'
+  },
+
+  lineSize: {
+    width: '100%',
+    marginVertical: 30
+  },
+
+  drawerLinks: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5
+  },
+
+  drawerIcons: {
+    width: 30,
+    height: 30
   }
+
 
 });
