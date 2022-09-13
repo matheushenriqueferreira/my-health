9724237ee87835c0b97a6431fdf608fd45220030
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-13 18:09:50 -0300

    Alterado TouchableOpacity para View

--- components/ButtonComponent/index.js
+++ components/ButtonComponent/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import { View, Text } from "react-native";
 import { ButtonComponentStyle } from "./styles";
 
 
 const ButtonComponent = ({btnText, btnColor}) => {
   return(
-    <TouchableOpacity style={[ButtonComponentStyle.btnDisplay, {backgroundColor: btnColor}]}>
+    <View style={[ButtonComponentStyle.btnDisplay, {backgroundColor: btnColor}]}>
       <Text style={ButtonComponentStyle.btnTextStyle}>{btnText}</Text>
-    </TouchableOpacity>
+    </View>
   );
 }
 
