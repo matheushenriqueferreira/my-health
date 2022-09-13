a1d689674c15c31b097dae83a26261245d971afc
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-13 18:09:10 -0300

    Componente dinamico e remoção do drawer

--- components/NavbarComponent/index.js
+++ components/NavbarComponent/index.js
@@ -6,34 +6,23 @@ import line from '../../assets/image/line.png'
 import iconVaccine from '../../assets/image/icon-vaccine.png'
 import iconLogout from '../../assets/image/logout.png'
 
-const NavbarComponent = () => {
-  
-  const handleDrawerNavigator = () => {
-    
-  }
+const NavbarComponent = ({status, navbarText}) => {
   
   return(
     <View style={NavbarComponentStyle.navbar}>
       <View style={NavbarComponentStyle.navbarContainer}>
         <View style={[NavbarComponentStyle.displayFlex]}>
-          <TouchableOpacity onPress={() => handleDrawerNavigator()}>
-            <Image style={NavbarComponentStyle.hamburgerIcon} source={hamburgerIcon} />
-          </TouchableOpacity>
-          <Text style={NavbarComponentStyle.title}>Minhas vacinas</Text>
-        </View>
-      </View>
-      <View style={NavbarComponentStyle.drawerNavigator}>
-        <View style={NavbarComponentStyle.drawerNavigatorContainer}>
-          <Text style={[NavbarComponentStyle.drawerText, NavbarComponentStyle.textCenter]}>Olá Matheus</Text>
-          <Image source={line} style={NavbarComponentStyle.lineSize}/>
-          <TouchableOpacity style={NavbarComponentStyle.drawerLinks}>
-            <Image source={iconVaccine} style={NavbarComponentStyle.drawerIcons}/>
-            <Text style={NavbarComponentStyle.drawerText}>Minhas vacinas</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={NavbarComponentStyle.drawerLinks}>
-            <Image source={iconLogout} style={NavbarComponentStyle.drawerIcons}/>
-            <Text style={NavbarComponentStyle.drawerText}>Sair</Text>
-          </TouchableOpacity>
+          {
+            status === 'notLogged' ?
+            <View>
+              <Image style={NavbarComponentStyle.navbarIcon} source={iconVaccine} />
+            </View>
+            :
+            <TouchableOpacity>
+              <Image style={NavbarComponentStyle.navbarIcon} source={hamburgerIcon} />
+            </TouchableOpacity>
+          }
+          <Text style={NavbarComponentStyle.title}>{navbarText}</Text>
         </View>
       </View>
     </View>
