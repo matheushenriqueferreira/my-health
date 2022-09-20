187819e456be25e7a5a8948daa7b80f8d48beaf2
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-20 17:49:02 -0300

    Definido estrutura e estilo

--- components/CardVaccine/index.js
+++ components/CardVaccine/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { CardVaccineStyle } from "./styles";
-import { View } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import test from '../../assets/image/image-comprovante.png'
 
 const CardVaccine = () => {
   return(
-    <View>
-
-    </View>
+    <TouchableOpacity style={CardVaccineStyle.main}>
+      <View style={CardVaccineStyle.container1}>
+        <Text style={[CardVaccineStyle.textColorBlue, CardVaccineStyle.name]}>BCG</Text>
+        <Text style={CardVaccineStyle.dose}>Dose única</Text>
+        <Text style={[CardVaccineStyle.textColorBlue, CardVaccineStyle.date]}>11/06/2022</Text>
+      </View>
+      <View style={CardVaccineStyle.container2}>
+        <Image style={CardVaccineStyle.image} resizeMode='stretch' source={test} />
+      </View>
+      <View style={CardVaccineStyle.container3}>
+        <Text style={CardVaccineStyle.container3Text}>Não há próxima dose</Text>
+      </View>
+    </TouchableOpacity>
   );
 }
 
--- components/CardVaccine/styles.js
+++ components/CardVaccine/styles.js
@@ -1,5 +1,68 @@
 import { StyleSheet } from "react-native";
+import { color } from "react-native-reanimated";
 
 export const CardVaccineStyle = StyleSheet.create({
+  main: {
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 10,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    borderRadius: 8,
+    width: '50%',
+    height: 200,
+    maxWidth: '50%',
+    maxHeight: 200
+  },
 
+  textColorBlue: {
+    color: '#3F92C5'
+  },
+
+  name: {
+    fontSize: 26
+  },
+
+  dose: {
+    backgroundColor: '#3F92C5',
+    fontSize: 10,
+    paddingHorizontal: 8,
+    color: '#FFFFFF'
+  },
+
+  date: {
+    color: '#8B8B8B',
+    fontSize: 10
+  },
+
+  container1: {
+    justifyContent: 'space-between',
+    height: 70,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+
+  container2: {
+    width: 175,
+    maxWidth: 175,
+    alignItems: 'center',
+    height: 120,
+    justifyContent: 'center'
+  },
+
+  image: {
+    width: 230,
+    maxWidth: 230,
+    height: 100,
+    maxHeight: 100,
+  },
+
+  container3: {
+    alignItems: 'flex-end'
+  },
+
+  container3Text: {
+    color: '#FD7979',
+    fontSize: 10
+  }
 });
