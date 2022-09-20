3f89df619b5b0b965a0ae9fdace5ecbe02b89edf
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-20 18:56:54 -0300

    Definido estrutura da Home

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { View, TextInput, Image, TouchableOpacity } from "react-native";
+import CardVaccine from "../../components/CardVaccine";
 import { HomeStyle } from "./styles";
-
+import iconSearch from '../../assets/image/search.png'
+import ButtonComponent from '../../components/ButtonComponent'
+import NavbarComponent from '../../components/NavbarComponent'
 
 const Home = () => {
   return(
     <>
-
+    <NavbarComponent status={'logged'} navbarText='Minhas vacinas' />
+      <View style={HomeStyle.main}>
+        <View style={HomeStyle.searchContainer}>
+          <Image style={HomeStyle.searchIcon} resizeMode='stretch' source={iconSearch} />
+          <TextInput style={HomeStyle.inputStyle} placeholder="PESQUISAR VACINA..." placeholderTextColor={'#8B8B8B'}/>
+        </View>
+        <View style={HomeStyle.vaccineList}>
+          <CardVaccine />
+          <CardVaccine />
+          <CardVaccine />
+        </View>
+        <TouchableOpacity style={HomeStyle.btnContainer}>
+          <ButtonComponent  btnText={'Nova vacina'} btnColor={'#37BD6D'}/>
+        </TouchableOpacity>
+      </View>
     </>
   );
 }
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -1,5 +1,43 @@
 import { StyleSheet } from "react-native";
 
 export const HomeStyle = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: '#ADD4D0',
+    paddingHorizontal: 15,
+    justifyContent: 'space-around'
+  },
 
+  searchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    height: 40,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 3
+  },
+
+  searchIcon: {
+    width: 20,
+    height: 20,
+    marginHorizontal: 10
+  },
+
+  inputStyle: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+    color: '#3F92C5'
+  },
+
+  vaccineList: {
+    flex: 0.8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap'
+  },
+
+  btnContainer: {
+    alignItems: 'center'
+  }
 });
