e6bdf34a1669402bb265a5b785147ee86a36b9a8
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-20 16:38:42 -0300

    criado arquivos do component CardVaccine

---
+++ components/CardVaccine/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { CardVaccineStyle } from "./styles";
+import { View } from "react-native";
+
+const CardVaccine = () => {
+  return(
+    <View>
+
+    </View>
+  );
+}
+
+export default CardVaccine;
