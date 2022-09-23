1c1e398b8a7d1e865bac8d24258e63459a0ab01b
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-23 17:23:16 -0300

    Ajustado import e component inicial

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import ScreenNavigation from './config/ScreenNavigation';
+import NoLoggedNavigation from './config/NoLoggedNavigation'
 
-AppRegistry.registerComponent(appName, () => ScreenNavigation);
+AppRegistry.registerComponent(appName, () => NoLoggedNavigation);
