128ef715d108d815eda5a9cf52be29c7dd2ff6e4
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-23 17:27:30 -0300

    Criado e estilizado screen NextVaccines

--- screens/NextVaccines/index.js
+++ screens/NextVaccines/index.js
@@ -1,9 +1,25 @@
 import React from "react";
-import NextVaccinesStyle from './styles';
+import { View, TouchableOpacity } from "react-native";
+import {NextVaccinesStyle} from './styles';
+import NavbarComponent from '../../components/NavbarComponent';
+import NextVaccinesComponent from "../../components/NextVaccinesComponent";
+import ButtonComponent from '../../components/ButtonComponent';
 
-const NextVaccines = () => {
+const NextVaccines = ({navigation}) => {
   return(
     <>
+      <NavbarComponent navigation={navigation} navbarText={'Próximas vacinas'} status={'logged'} />
+      <View style={NextVaccinesStyle.main}>
+        <View style={NextVaccinesStyle.mainContainer1}>
+          <NextVaccinesComponent />
+          <NextVaccinesComponent />
+        </View>
+        <View style={NextVaccinesStyle.mainContainer2}>
+          <TouchableOpacity>
+            <ButtonComponent btnText={'Nova vacina'} btnColor={'#37BD6D'} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 }
--- screens/NextVaccines/styles.js
+++ screens/NextVaccines/styles.js
@@ -1,5 +1,20 @@
 import { StyleSheet } from "react-native";
 
 export const NextVaccinesStyle = StyleSheet.create({
+  main: {
+    flex: 1,
+    paddingHorizontal: 15,
+    backgroundColor: '#ADD4D0',
+    justifyContent: 'space-around'
+  },
 
+  mainContainer1: {
+    flex: 0.8,
+  },
+
+  mainContainer2: {
+    flex: 0.1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 });
