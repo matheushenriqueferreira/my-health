d6e67f82d4a94b815bf6552455f9b86a240ccba5
matheushenriqueferreira <maatheus27silva@gmail.com>
2022-09-26 19:01:34 -0300

    Ajustado FirebaseConfig para uso do firestore

--- config/FirebaseConfig/firebase.js
+++ config/FirebaseConfig/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-
+import { initializeFirestore, getFirestore } from "firebase/firestore";
+''
 const firebaseConfig = {
   apiKey: "AIzaSyB32kFHA-wkZ_41F9C8qAxvGTWn0Wc45Tk",
   authDomain: "movel-e8b0c.firebaseapp.com",
@@ -10,5 +11,6 @@ const firebaseConfig = {
   appId: "1:793910504736:web:22cbbe5572d6b397ac3b45"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const firebase = initializeApp(firebaseConfig);
+export const auth = getAuth(firebase);
+export const firestore = initializeFirestore(firebase, {experimentalForceLongPolling: true});
