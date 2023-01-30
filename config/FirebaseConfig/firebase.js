f9bea8639aea55220806652d00eb25c97070a25f
matheushenriqueferreira <maatheus27silva@gmail.com>
2023-01-30 20:48:42 -0300

    Removido configurações obsoletas

--- config/FirebaseConfig/firebase.js
+++ config/FirebaseConfig/firebase.js
@@ -1,14 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { initializeFirestore, getFirestore } from "firebase/firestore";
-''
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB32kFHA-wkZ_41F9C8qAxvGTWn0Wc45Tk",
-  authDomain: "movel-e8b0c.firebaseapp.com",
-  projectId: "movel-e8b0c",
-  storageBucket: "movel-e8b0c.appspot.com",
-  messagingSenderId: "793910504736",
-  appId: "1:793910504736:web:22cbbe5572d6b397ac3b45"
+  // Insira aqui a configuração do projeto do Firebase
 };
 
 export const firebase = initializeApp(firebaseConfig);
